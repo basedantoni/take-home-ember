b6fe599198480a940251e0bbab956a66d900b1f0
Anthony Mercado <antho@Kendyls-MacBook-Air.local>
2024-11-10 13:38:03 -0600

    search dogs functionality

--- app/controllers/dogs.ts
+++ app/controllers/dogs.ts
@@ -3,13 +3,33 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import type DogModel from 'voyant-take-home/models/dog';
 
 export default class DogsController extends Controller {
+  declare model: DogModel[];
+
   @service declare store: Store;
 
   @tracked isAdding = false;
   @tracked isEditing = false;
   @tracked errorMessage = '';
+  @tracked searchTerm = '';
+
+  get filteredDogs() {
+    console.log('filteredDogs', this.searchTerm);
+    if (!this.searchTerm.trim()) {
+      // If searchTerm is empty, return all dogs
+      return this.model;
+    }
+
+    return this.model.filter((dog) => this.isMatch(dog));
+  }
+
+  @action
+  updateSearchTerm(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.searchTerm = target.value;
+  }
 
   @action
   toggleAdd() {
@@ -31,5 +51,17 @@ export default class DogsController extends Controller {
   @action
   setErrorMessage(message: string) {
     this.errorMessage = message;
+  }
+
+  // Helper function to check if a dog matches the search term
+  private isMatch(dog: any) {
+    let term = this.searchTerm.toLowerCase();
+    return (
+      dog.name.toLowerCase().includes(term) ||
+      dog.breed.toLowerCase().includes(term) ||
+      dog.owner.toLowerCase().includes(term) ||
+      dog.size.toLowerCase().includes(term) ||
+      dog.description.toLowerCase().includes(term)
+    );
   }
 }
